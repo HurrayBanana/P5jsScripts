e2467fe518e20b4e32cf4f8b1c4dc8e3411a1291
Hurray Banana <HurrayBanana@users.noreply.github.com>
2022-01-14 12:30:02 +0000

    Update and rename Information.txt to Information.js

--- DemoCodingExercises/Task1/Information.js
+++ DemoCodingExercises/Task1/Information.js
@@ -1,13 +1,12 @@
+/*
 You have been asked to write a function called segment that accepts 3 parameters 
 
 A string called mess (of at least 20 characters) 
-
 A width (number of characters to display) must be <20 and >10 
-
 A start index between 0 and the length of the string -1 
 
 It should return a string of exactly width characters which contains the portion of the string starting from start index. If there are not enough characters, repeat from the start of the mess string 
-
+*/
 Example 1 
 
 segment(“DAVEBLOKE”, 5, 3) returns “LOK” 
