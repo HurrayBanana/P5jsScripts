95d9dba0b21bcb11dad23fc57f9d9c0890503733
Hurray Banana <HurrayBanana@users.noreply.github.com>
2022-01-14 14:57:47 +0000

    Update and rename information.js to information.md

--- DemoCodingExercises/scroller/task3/information.js
+++
@@ -1,8 +0,0 @@
-/*
-The scrolling moves too fast, we need to slow it down
-remember the draw() function runs 60 times a second, which calls the logicSimple() function each time
-*/
-The logicSimple() function updates the start position p along the message to display
-
-make a modification to the logicSimple() function to implement a delay counter so that p is not incremented every time the function is called
-
