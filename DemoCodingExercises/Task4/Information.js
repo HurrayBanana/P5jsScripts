e6baf6ccb3a9522ebd99c38cbb2af78c6193ab9b
Hurray Banana <HurrayBanana@users.noreply.github.com>
2022-01-14 14:32:20 +0000

    Update and rename Information.md to Information.js

--- DemoCodingExercises/Task4/Information.js
+++ DemoCodingExercises/Task4/Information.js
@@ -7,12 +7,12 @@ Add the  angle variable and waveMessage() function to your skectch
 
 and add a call in the draw function to call the waveMessage function
 */
-<p>
-<code>
+
 waveMessage(m, 0, 200)
 
-
+/*
 You may wish to comment the line in logicSimple() that increments p so you can concentrate on the shap of the wave
+*/
 
 function logicSimple(){
   delay++;
@@ -23,7 +23,8 @@ function logicSimple(){
       p = 0;
   }
 }
-</code>
-</p>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/os5Dzn3-0NA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+
+
+
 
