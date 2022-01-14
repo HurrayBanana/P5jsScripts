effdc8702e568763d36a7586bde22a7323c38544
Hurray Banana <HurrayBanana@users.noreply.github.com>
2022-01-14 14:13:03 +0000

    Update Information.js

--- DemoCodingExercises/Task4/Information.js
+++ DemoCodingExercises/Task4/Information.js
@@ -11,3 +11,15 @@ and add a call in the draw function to call the waveMessage function
 waveMessage(m, 0, 200)
 
 
+You may wish to comment the line in logicSimple() that increments p so you can concentrate on the shap of the wave
+
+function logicSimple(){
+  delay++;
+  if (delay % 5 == 0)
+  {
+    //p++
+    if (p == message.length)
+      p = 0;
+  }
+}
+
