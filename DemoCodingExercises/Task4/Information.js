f86f16f031d03b3acf6c658f0d742c77ebcb295f
Hurray Banana <HurrayBanana@users.noreply.github.com>
2022-01-14 12:58:21 +0000

    Create Information.js

---
+++ DemoCodingExercises/Task4/Information.js
@@ -0,0 +1,13 @@
+/*
+The lead coder as added a more funky way of drawing the pportion of the message
+
+he wants you to experiment with it to get a pleasing affect.
+
+Add the  angle variable and waveMessage() function to your skectch
+
+and add a call in the draw function to call the waveMessage function
+*/
+
+waveMessage(m, 0, 150)
+
+
