9295df5169eba1dd24452f7f99a6a4910f2b0a83
Hurray Banana <HurrayBanana@users.noreply.github.com>
2022-04-21 07:47:24 +0100

    Update p5hashtables.js

--- DataStructures/p5hashtables.js
+++ DataStructures/p5hashtables.js
@@ -1,37 +1,48 @@
 let hash;
 let messages = [];
+//constants
 const EMPTY = "< -- >";
 const NULL = -1;
 const MESSAGE = 0;
 const LINK = 1;
 const NEXT = "next available position";
 const OVERFLOW = "overflow";
-
+//globals
 let lineDrop = 11;
 let mode = OVERFLOW;
 let hashtable = new Array(20);
 let overflowtable = new Array(20)
 let line = 20;
 let messageToProcess = 0;
-let tablesize;
+let maxentries = 50;
+let freespace = 0;
+let count = 0;
+let adding = "";
 /* UI elements */
 let canv;
 let slspeed;
 let rdmethod;
 let tbwords;
 
+
+function setup() {
+    canv = createCanvas(550, 800);
+    createUI();
+    init();
+}
+
 function createUI(){
   slspeed = createSlider(0,180, 120);
   slspeed.position(10,10);
   slspeed.value(0);
+
   rdmethod = createRadio();
   rdmethod.option('0','Next available position');
   rdmethod.option('1','overflow');
   rdmethod.selected('1');
   rdmethod.position(200,10);
-  //rdmethod.id('rdmethod');
   rdmethod.mouseClicked(methodchanged);
-  //tbwords = createInput();
+
   tbwords = createElement('textarea');
   tbwords.attribute('rows',3);
   tbwords.style('resize','none');
@@ -40,6 +51,8 @@ function createUI(){
   tbwords.value('Enter a series of words. Each one will be entered into the hash table, duplicates will be ignored, maximum unique words is 100');
   tbwords.input(typed);
 }
+
+//picks up text entry interaction, stops data addition
 function typed(){
   //stop processing if we are entering stuff
   slspeed.value(0);
@@ -51,9 +64,10 @@ function methodchanged(){
   else
     mode = OVERFLOW;
   init();
-  setuptables();
-  console.log(rdmethod.value());
+  if (slspeed.value() == 0)
+    slspeed.value(1);
 }
+
 /* perform hash function on message 
  Need to mod by table size to keep in range
 */
@@ -65,41 +79,37 @@ function hashString(stringValue, scale){
     return h % scale;
 }
 
-function setup() {
-    canv = createCanvas(550, 800);
-    createUI();
-    init();
-    setuptables();
-    //processMessages();
-}
-
 function init(){
+  setuptables();
   freespace = 0;
   count = 0;
-  seconds = 0;
   adding = "";
   messageToProcess = 0;
   createMessages();
 }
+
 function createMessages(){
   let m = tbwords.value();
-  m = m.replace(',','').replace('.','');
+  m = m.replace(/,/gm,' ').replace('.',' ').replace(/\r?\n|\r/gm,' ');
   m = m.split(' ');
   messages = [];
-  for (let p = 0; p < m.length; p++){
-    if (messages.indexOf(m[p]) == -1)
+  
+  //only add words that we have space for
+  for (let p = 0; p < m.length && messages.length < maxentries; p++){
+    //m[p] = m[p].replace(/\s/g,'');
+    if (m[p] != '' && messages.indexOf(m[p]) == -1)
       messages.push(m[p]);
   }
 }
 function setuptables(){
     let size = 50;
+    maxentries = size*2;
     //double space for nex available position (so equivalent to overflow)
     if (mode == NEXT) 
         size *= 2;
-    tablesize = size;
   
     hashtable = new Array(size);
-    overflowtable = new Array(size);
+    overflowtable = new Array(1);
 
     for (let p = 0; p < hashtable.length; p++) {
         let b = []
@@ -107,24 +117,16 @@ function setuptables(){
         b.push(NULL);
         hashtable[p] = b;
     }
-    for (let p = 0; p < overflowtable.length; p++) {
+    
+    for (let p = 0; p < 1/*overflowtable.length*/; p++) {
         let b = []
         b.push(EMPTY);
         b.push(NULL);
         overflowtable[p] = b;
     }
-}
-/* process all messages in one go */
-function processMessages(){
-  for (let p = 0; p < messages.length; p++){
-    thishash = hashString(messages[p], hashtable.length);
     
-    if (hashtable[thishash][MESSAGE] == EMPTY)
-      storehash(thishash, messages[p], NULL);
-    else
-      handleCollision(thishash, messages[p]);
-  }
 }
+
 /* processes a particular message adding it to the hash table*/
 function processThisMessage(index){
     thishash = hashString(messages[index], hashtable.length);
@@ -135,12 +137,13 @@ function processThisMessage(index){
 }
 
 function storehash(loc, mess, link){
-  console.log("store@"+loc);
+  //console.log("store@"+loc);
   dave = [mess,link];
   hashtable[loc] = dave;
   hashtable[loc][MESSAGE] = mess
   hashtable[loc][LINK] = link;
 }
+
 function handleCollision(pos, message){
   if (mode == NEXT)
     nextAvailable(pos, message);
@@ -150,17 +153,24 @@ function handleCollision(pos, message){
 
 function overflow(pos, message){
     overflowloc = setoverflow(message);
-
-    if (hashtable[pos][LINK] == NULL)
-        hashtable[pos][LINK] = overflowloc;
-    else
-        overflowtable[overflowloc][LINK] = hashtable[pos][LINK];
-        hashtable[pos][LINK] = overflowloc;
+    if (overflowloc < overflowtable.length){
+      if (hashtable[pos][LINK] == NULL)
+          hashtable[pos][LINK] = overflowloc;
+      else
+          overflowtable[overflowloc][LINK] = hashtable[pos][LINK];
+          hashtable[pos][LINK] = overflowloc;
+    }
 }
-let freespace = 0;
+
 function setoverflow(message){
-    overflowtable[freespace][MESSAGE] = message;
-    return freespace++;
+  if (freespace == overflowtable.length){
+    let b = []
+    b.push(EMPTY);
+    b.push(NULL);
+    overflowtable.push(b);
+  }
+  overflowtable[freespace][MESSAGE] = message;
+  return freespace++;
 }
 function nextAvailable(pos, message){
   count = 0;
@@ -182,31 +192,19 @@ function nextAvailable(pos, message){
     //hashtable[searchPos][MESSAGE] = message + " [collision]";
 }
 function appendhash(loc, mess, link){
-    console.log("store@"+loc);
+    //console.log("store@"+loc);
     dave = [mess,link];
     hashtable[loc] = dave;
     hashtable[loc][MESSAGE] += mess
     hashtable[loc][LINK] = link;
   }
-let count = 0;
-let seconds = 0;
-let adding = "";
+
 function logic(){
-    dotime();
-    //add another every 2 seconds
-//    if (messageToProcess < messages.length && count == 0 && seconds % 2 == 0)
+    count++;
     if (messageToProcess < messages.length && count % slspeed.value() == 0)
     {
         adding = messages[messageToProcess];
         processThisMessage(messageToProcess++);
-    }
-}
-
-function dotime(){
-    count ++;
-    if (count == 60){
-        seconds++;
-        //count = 0;
     }
 }
 
@@ -215,7 +213,7 @@ function draw() {
     background(220);
     drawSpeed();
     line = 100;
-    write("Last message added [" + adding + "]", 10,0);
+    write("Last word added [" + adding + "] " + messageToProcess + " of " + messages.length + " unique words", 10,0);
     line = 120;
     if (mode == OVERFLOW) 
         OverflowDisplay();
@@ -233,6 +231,7 @@ function drawSpeed(){
     m = "add every " + (floor(v/60)==0?"":floor(v/60)) + ((v % 60==0) ? " sec ": " " + v % 60 + "//60 th sec");
   text(m, slspeed.position().x + dw * v, slspeed.position().y);
 }
+
 function OverflowDisplay(){
     h = line;
     write("SYMBOL TABLE", 10,lineDrop*2);
@@ -240,6 +239,8 @@ function OverflowDisplay(){
     write("Link", 50,0);
     write("Contents", 100,lineDrop);
     for (let p = 0; p < hashtable.length; p++){
+      fill((hashtable[p][LINK]==NULL)?(hashtable[p][MESSAGE]==EMPTY?'grey':'black'):'blue');
+      
       write(p, 10, 0);
       write((hashtable[p][LINK]==NULL?"NULL":hashtable[p][LINK]), 50, 0);
       write(hashtable[p][MESSAGE], 100, lineDrop);
@@ -251,11 +252,11 @@ function OverflowDisplay(){
     write("Contents", 300,lineDrop);
     
     for (let p = 0; p < overflowtable.length; p++){
+      fill((overflowtable[p][LINK]==NULL && overflowtable[p][MESSAGE] != EMPTY)?'red':'black');
       write(p, 210, 0);
       write((overflowtable[p][LINK]==NULL?"NULL":overflowtable[p][LINK]), 250, 0);
-      write(overflowtable[p][MESSAGE], 300, lineDrop);
+      write(overflowtable[p][MESSAGE] + (overflowtable[p][LINK]==NULL && overflowtable[p][MESSAGE] != EMPTY? "    <- end of this overflow list":""), 300, lineDrop);
     }
-    
 }
 
 function NextposDisplay(){
@@ -264,8 +265,10 @@ function NextposDisplay(){
     write("Contents", 50,lineDrop);
     h = line;
     for (let p = 0; p < hashtable.length; p++){
-        if (p == tablesize/2)
+        if (p == maxentries/2)
           line = h;
+        fill((hashtable[p][MESSAGE].indexOf('[collision @')!= -1)?'red':(hashtable[p][MESSAGE]==EMPTY?'grey':'black'));
+
         write(p, 10 + 210 * floor(p/50), 0);
         write(hashtable[p][MESSAGE], 50+ 210 * floor(p/50), lineDrop);
     }
